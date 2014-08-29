5c48cdf6175c38bb935ed064deff3bcdb4954929
Yuichiro Mukai <y.iky917@gmail.com>
2014-08-29 11:13:07 +0900

    スライド載せた

--- plugins/slideshare.rb
+++ plugins/slideshare.rb
@@ -1,4 +1,3 @@
-RawBlameHistory   
 module Jekyll
   class SlideShare < Liquid::Tag
 
@@ -8,7 +7,7 @@ module Jekyll
     end
 
     def render(context)
-      %(<iframe src="http://www.slideshare.net/slideshow/embed_code/#{@id}" width="595" height="446" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen></iframe> )
+      %(<iframe src="http://www.slideshare.net/slideshow/embed_code/#{@id}" width="510" height="420" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="margin-top: 0px !important; border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen></iframe> )
     end
   end
 end
